b87821b459d4af6a330c0385ac0fcfb56870916a
YuanHao-7684 <61580825+YuanHao-7684@users.noreply.github.com>
2022-07-07 18:13:06 +0100

    Add files via upload
    Update the path file to achieve page jumping

--- mywbsite/urls.py
+++ mywbsite/urls.py
@@ -24,5 +24,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.index,name='index'),
     path('signup/', views.signup, name='signup'),
-    path('homepage/', views.homepage, name='homepage')
+    path('homepage/', views.homepage, name='homepage'),
+    path('patternpage/',views.patternpage,name='patternpage'),
+    path('cases/',views.cases,name='cases'),
+    path('View/',views.View,name='View'),
+    path('Report/',views.Report,name='Report'),
+    path('Log/',views.Log,name='Log'),
 ]
