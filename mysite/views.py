6a41cc93ef211de84dfea6a895fc479709317beb
YuanHao-7684 <61580825+YuanHao-7684@users.noreply.github.com>
2022-07-07 18:12:14 +0100

    Add files via upload
    upload views file to realize page jump between various pages. (*I wanted to use the anchor point to jump to the unified page, but found the obvious problem of passing parameters, considering that the tasks of each page need to interact independently.Detached these pages as separate html files)

--- mysite/views.py
+++ mysite/views.py
@@ -2,24 +2,61 @@ from django.shortcuts import render
 from mysite import models
 # Create your views here.
 from django.shortcuts import HttpResponse
-messagelist=[]
+
+messagelist = []
+currentuser = ""
+
 
 def index(request):
     return render(request, 'index.html')
+
+
 def signup(request):
     messagelist.clear()
     if request.method == "POST":
         username = request.POST.get("newname", None)
         password = request.POST.get("newpassword", None)
         email = request.POST.get("newemail", None)
-        flag=models.User.objects.get_or_create(user=username, email=email, password=password)
-    if(flag==True):
-        truemessage={"message":"you have create a new account"}
+        flag = models.User.objects.get_or_create(user=username, email=email, password=password)
+    if (flag == True):
+        truemessage = {"message": "you have create a new account"}
         messagelist.append(truemessage)
         return render(request, 'index.html')
     else:
-        failmessage={"message":"you already have a account,log in now"}
+        failmessage = {"message": "you already have a account,log in now"}
         messagelist.append(failmessage)
         return render(request, 'index.html')
+
+
 def homepage(request):
-    return render(request,'homepage.html')
+    currentuser = "Hao Yuan"
+    return render(request, 'homepage.html', {'user': currentuser})
+
+
+def patternpage(request):
+    currentuser = "Hao Yuan"
+    return render(request, 'list pages.html', {'user': currentuser})
+
+
+
+
+
+# case management
+def cases(request):
+    currentuser = "Hao Yuan"
+    return render(request, 'casepage.html', {'user': currentuser})
+
+
+def View(request):
+    currentuser = "Hao Yuan"
+    return render(request, 'caseView.html', {'user': currentuser})
+
+
+def Log(request):
+    currentuser = "Hao Yuan"
+    return render(request, 'caseLog.html', {'user': currentuser})
+
+
+def Report(request):
+    currentuser = "Hao Yuan"
+    return render(request, 'caseReport.html', {'user': currentuser})
