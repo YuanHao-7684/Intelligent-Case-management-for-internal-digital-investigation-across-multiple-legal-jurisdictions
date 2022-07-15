b3ed56a45ab596a12176a7c96db1b843a91047e2
YuanHao-7684 <61580825+YuanHao-7684@users.noreply.github.com>
2022-07-15 19:04:59 +0100

    Add files via upload
    Modified the model and added two new properties: 1. Restricted Mode 2. Related Countries

--- mysite/models.py
+++ mysite/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
-class User (models.Model):
+class User(models.Model):
     user = models.CharField(max_length=32)
     password = models.CharField(max_length=16)
     email = models.CharField(max_length=32)
-class Law (models.Model):
+
+class LawIno(models.Model):
     country = models.CharField(max_length=64)
     LawName = models.CharField(max_length=128)
     KeyPoint = models.CharField(max_length=1024)
+    restrictedMode = models.CharField(max_length=16)
+    AgreementCountry = models.CharField(max_length=1024, blank=True, null=True)
